8d66cdb411347591af2b4807e9f3e19cf7739c5a
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-04-04 22:00:00 +0530

    remove unnecessary component

--- apps/fireflow-client/src/app/app.component.ts
+++ apps/fireflow-client/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
+  imports: [ RouterModule ],
+  selector: 'fireflow-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
