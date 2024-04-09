f35080c18ccd8174c5e5ab129fe39d2da22215d2
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-04-09 22:00:00 +0530

    add sql generation service

--- apps/fireflow-api/src/app/gen-sql/gen-sql.service.ts
+++ apps/fireflow-api/src/app/gen-sql/gen-sql.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { GenerativeModel, GoogleGenerativeAI } from '@google/generative-ai';
+import { SQLPromptDTO } from '../_shared/dto/sql-prompt.dto';
 
 @Injectable()
-export class GenSqlService {}
+export class GenSqlService {
+    genAI: GoogleGenerativeAI;
+    model: GenerativeModel;
+
+    constructor(private _configService: ConfigService) {
+        this.genAI = new GoogleGenerativeAI(_configService.get('API_KEY'));
+        this.model = this.genAI.getGenerativeModel({ model: "gemini-pro" });
+    }
+
+    async getResponse(promptRequest: SQLPromptDTO): Promise<string> {
+        const result = await this.model.generateContent(promptRequest.content);
+        const response = await result.response;
+        const text = response.text();
+        return text;
+    }
+}
