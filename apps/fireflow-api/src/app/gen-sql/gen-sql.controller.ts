1853979134844ead0a9f77cdd24d0027cdedcef2
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-04-10 22:00:00 +0530

    add sql generation controller

--- apps/fireflow-api/src/app/gen-sql/gen-sql.controller.ts
+++ apps/fireflow-api/src/app/gen-sql/gen-sql.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { GenSqlService } from './gen-sql.service';
+import { SQLGeneratedDTO, SQLPromptDTO } from '../_shared/dto/sql-prompt.dto';
 
 @Controller('gen-sql')
 export class GenSqlController {
-  constructor(private readonly genSqlService: GenSqlService) {}
+  constructor(private readonly genSqlService: GenSqlService) { }
+
+  @Post()
+  generateText(@Body() promptRequest: SQLPromptDTO): Promise<SQLGeneratedDTO> {
+    return new Promise((resolve, reject) => {
+      this.genSqlService.getResponse(promptRequest).then((res) => {
+        const data: SQLGeneratedDTO = {
+          content: res
+        }
+        resolve(data);
+      }).catch((err) => {
+        reject(err);
+      });
+    });
+  }
 }
