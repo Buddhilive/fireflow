d1ea2a612ecd1bb45276dadeb0b76c2f73782600
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-04-13 22:00:00 +0530

    add syntax highliter component

--- apps/fireflow-client/src/app/app.config.ts
+++ apps/fireflow-client/src/app/app.config.ts
@@ -1,7 +1,16 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        fullLibraryLoader: () => import('highlight.js')
+      }
+    }
+  ],
 };
--- apps/fireflow-client/src/app/sql-generator/sql-generator.component.ts
+++ apps/fireflow-client/src/app/sql-generator/sql-generator.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HighlightModule } from 'ngx-highlightjs';
 
 @Component({
   selector: 'fireflow-sql-generator',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    HighlightModule
+  ],
   templateUrl: './sql-generator.component.html',
   styleUrl: './sql-generator.component.scss',
 })
-export class SqlGeneratorComponent {}
+export class SqlGeneratorComponent {
+  code = 'SELECT * FROM users';
+  languages = ['sql'];
+}
