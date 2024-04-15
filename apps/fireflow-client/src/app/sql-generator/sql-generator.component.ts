e9f6830a1a454b970e2d1cfa07ac3af1ac08944a
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-04-15 22:00:00 +0530

    implement generate function

--- apps/fireflow-client/src/app/sql-generator/sql-generator.component.ts
+++ apps/fireflow-client/src/app/sql-generator/sql-generator.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HighlightModule } from 'ngx-highlightjs';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'fireflow-sql-generator',
@@ -9,7 +10,8 @@ import { FormsModule } from '@angular/forms';
   imports: [
     CommonModule,
     HighlightModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   templateUrl: './sql-generator.component.html',
   styleUrl: './sql-generator.component.scss',
@@ -18,4 +20,17 @@ export class SqlGeneratorComponent {
   code = 'SELECT * FROM users';
   languages = ['sql'];
   prompt = '';
+
+  constructor(private httpClient: HttpClient) { }
+
+  generateQuery() {
+    if (this.prompt.length > 0) {
+      this.httpClient.post('/api/gen-sql', { content: this.prompt }).subscribe({
+        next: (data: any) => {
+          this.code = data.content;
+        },
+        error: (err) => console.log(err)
+      });
+    }
+  }
 }
