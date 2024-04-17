ac18597c4cf1345491b00d8045860fdd30070a46
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-04-17 22:00:00 +0530

    improve the prompt response

---
+++ apps/fireflow-client/src/app/_shared/interfaces/sql-prompt.interface.ts
@@ -0,0 +1,3 @@
+export interface ISQLPrompt {
+    content: string
+}
--- apps/fireflow-client/src/app/sql-generator/sql-generator.component.ts
+++ apps/fireflow-client/src/app/sql-generator/sql-generator.component.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HighlightModule } from 'ngx-highlightjs';
 import { FormsModule } from '@angular/forms';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { ISQLPrompt } from '../_shared/interfaces/sql-prompt.interface';
 
 @Component({
   selector: 'fireflow-sql-generator',
@@ -27,11 +28,16 @@ export class SqlGeneratorComponent {
     if (this.prompt.length > 0) {
       const promptRequest = `provide the sql query for the following task: ${this.prompt}`;
       this.httpClient.post('/api/gen-sql', { content: promptRequest }).subscribe({
-        next: (data: any) => {
-          this.code = data.content;
+        next: (data) => {
+          const content = (data as ISQLPrompt).content;
+          this.code = this.removeCodeBlockMarkers(content);
         },
         error: (err) => console.log(err)
       });
     }
   }
+
+  private removeCodeBlockMarkers(str: string) {
+    return str.replace(/^```sql\s+|\s+```$/g, "");
+  }  
 }
