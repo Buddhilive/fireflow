4b0305aa98d9321cf929d0109fd9e839d6cf33f3
Buddhi Kavindra <visitbudkavin@gmail.com>
2024-05-10 17:57:15 +0530

    re arrange layout

--- apps/fireflow-client/src/app/components/layout/layout.component.ts
+++ apps/fireflow-client/src/app/components/layout/layout.component.ts
@@ -1,18 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'fireflow-layout',
   standalone: true,
   imports: [
     CommonModule,
-    RouterOutlet
+    RouterModule
   ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
 export class LayoutComponent {
+
+  @Input() menuItems = [
+    {
+      label: 'Home',
+      icon: 'pi pi-home',
+      link: '/'
+    }
+  ];
+  @Input() title = 'Home';
+
   toggleSideNav(sideNavComp: HTMLElement) {
     sideNavComp.classList.toggle('show');
   }
